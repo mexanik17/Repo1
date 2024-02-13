a6f9fd507aa2547721ef51bc73cb3bb29c7bf46f
Batyi Oyun <batyinavas@gmail.com>
2024-02-13 15:07:07 +0700

    add new project

---
+++ Repo1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Repo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год:");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (VisokosYear(year))
+            {
+                Console.WriteLine("{0} год - високосный.", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} год - не високосный.", year);
+            }
+
+            Console.ReadLine();
+        }
+
+        static bool VisokosYear(int year)
+        {
+            
+            return year % 4 == 0;
+        }
+    }
+}
